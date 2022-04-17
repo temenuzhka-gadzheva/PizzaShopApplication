074e4f1d36227d89a5e36abb35f6d7d63559a819
TemenuzhkaG <temenujka_211@abv.bg>
2022-04-17 23:02:01 +0300

    add users and admin functionality

---
+++ PizzaShop/PizzaShop.Core/Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaShop.Core.ViewModels.Pagings;
+using PizzaShop.Core.ViewModels.Users;
+using PizzaShop.Models.Data;
+using System;
+using System.Linq;
+
+
+namespace PizzaShop.Core.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly PizzaShopDbContext context;
+        public AdminController(PizzaShopDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AllUsers(IndexViewModel model)
+        {
+            model.Orders = this.context.Orders.ToList();
+
+            model.Pager = model.Pager ?? new PagerViewModel();
+
+            model.Pager.Page = model.Pager.Page <= 0
+                                       ? 1
+                                       : model.Pager.Page;
+
+            model.Pager.ItemsPerPage = model.Pager.ItemsPerPage <= 0
+                                        ? 3
+                                        : model.Pager.ItemsPerPage;
+
+            model.Pager.PagesCount = (int)Math.Ceiling(context.Users.Count() / (double)model.Pager.ItemsPerPage);
+
+            model.Users = context.Users
+                                     .OrderBy(i => i.Id)
+                                     .Skip(model.Pager.ItemsPerPage * (model.Pager.Page - 1))
+                                     .Take(3)
+                                     .ToList();
+
+            return this.View(model);
+        }
+    }
+}
---
+++ PizzaShop/PizzaShop.Core/ViewModels/Users/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using PizzaShop.Core.ViewModels.Pagings;
+using PizzaShop.Models.Models;
+using System.Collections.Generic;
+
+
+namespace PizzaShop.Core.ViewModels.Users
+{
+    public class IndexViewModel
+    {
+        public virtual ICollection<User> Users { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+        public PagerViewModel Pager { get; set; }
+    }
+}
